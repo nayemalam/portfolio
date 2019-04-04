3fd7582bbf8f0837f6648e2d55058f2b1e13dd6c
Nayem <nayem.alam@mail.mcgill.ca>
2019-04-04 11:05:28 -0400

    deploying to heroku

---
+++ navbar.php
@@ -0,0 +1,38 @@
+<div id="aside">
+    <div id="on-top">
+    <figure>
+        
+        <!-- <div id="avatar"><img src="./images/na-logo.png" alt="na-logo" class="image is-5by3"></div> -->
+    <figcaption>Nayem Alam</figcaption>
+        
+    </figure>
+    <div class="menu-toggler">
+        <div class="bar half start"></div>
+        <div class="bar"></div>
+        <div class="bar half end"></div>
+    </div>
+    </div>
+
+    <nav class="sidebar"> 
+        <div id="avatar"><img src="./images/favicon.png" alt="na-logo" class="image is-2by5"></div>
+        <h1 class="fig-caption">Nayem Alam</h1>
+        <div class="center-items">
+            <ul class="social-links">
+                <li><a href="#"><i class="fab fa-github fa-lg"></i></a></li>
+                <li><a href="#"><i class="fab fa-linkedin-in fa-lg"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram fa-lg"></i></a></li>
+                <li><a href="#"><i class="fab fa-twitter fa-lg"></i></a></li>
+            </ul>
+        </div>
+        <ul> 
+            <!-- <li><img src="./images/na-logo.png" alt="nayem-logo" class="logo-menu"/></li> -->
+            <li><a href="/index.html" class="nav-link">Home</a></li>
+            <li><a href="/pages/about.html" class="nav-link"> About</a></li>
+            <li><a href="#" class="nav-link">Portfolio</a></li>
+            <li><a href="#" class="nav-link">Media</a></li>
+            <li><a href="#" class="nav-link">Speaking</a></li>
+            <li><a href="#" class="nav-link">Say Hi!<span id="big">👋</span></a></li>
+        </ul>
+    </nav>
+    
+</div>
