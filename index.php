43ce0245daa47510b27579676641890b2ad45cd7
Nayem <nayem.alam@mail.mcgill.ca>
2019-04-09 11:01:57 -0400

    included php files

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
 <?php header( 'Location: /index.html' ) ;  ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include('head.php'); ?>
+    </head>
+    <body>
+        <?php include('navbar.php'); ?>
+    </body>
--- navbar.php
+++ navbar.php
@@ -2,7 +2,6 @@
     <div id="on-top">
     <figure>
         
-        <!-- <div id="avatar"><img src="./images/na-logo.png" alt="na-logo" class="image is-5by3"></div> -->
     <figcaption>Nayem Alam</figcaption>
         
     </figure>
@@ -14,19 +13,19 @@
     </div>
 
     <nav class="sidebar"> 
-        <div id="avatar"><img src="./images/favicon.png" alt="na-logo" class="image is-2by5"></div>
+        <div id="avatar"><img src="./images/favicon.png" alt="nayem-logo" class="image is-2by5"></div>
         <h1 class="fig-caption">Nayem Alam</h1>
         <div class="center-items">
             <ul class="social-links">
-                <li><a href="#"><i class="fab fa-github fa-lg"></i></a></li>
-                <li><a href="#"><i class="fab fa-linkedin-in fa-lg"></i></a></li>
-                <li><a href="#"><i class="fab fa-instagram fa-lg"></i></a></li>
-                <li><a href="#"><i class="fab fa-twitter fa-lg"></i></a></li>
+                <li><a href="https://github.com/nayemalam"><i class="fab fa-github fa-lg"></i></a></li>
+                <li><a href="https://www.linkedin.com/in/nayemalam/"><i class="fab fa-linkedin-in fa-lg"></i></a></li>
+                <li><a href="https://www.instagram.com/nayem_wizdom/"><i class="fab fa-instagram fa-lg"></i></a></li>
+                <li><a href="https://twitter.com/nayem_wizdom"><i class="fab fa-twitter fa-lg"></i></a></li>
             </ul>
         </div>
         <ul> 
-            <!-- <li><img src="./images/na-logo.png" alt="nayem-logo" class="logo-menu"/></li> -->
-            <li><a href="/index.html" class="nav-link">Home</a></li>
+        <li <?php if ($currentPage === 'Home') {echo 'class="active"';} ?>><a href="index">Home</a></li>     
+            <!-- <li><a href="/index.html" class="nav-link">Home</a></li> -->
             <li><a href="/pages/about.html" class="nav-link"> About</a></li>
             <li><a href="#" class="nav-link">Portfolio</a></li>
             <li><a href="#" class="nav-link">Media</a></li>
@@ -34,5 +33,4 @@
             <li><a href="#" class="nav-link">Say Hi!<span id="big">👋</span></a></li>
         </ul>
     </nav>
-    
 </div>
