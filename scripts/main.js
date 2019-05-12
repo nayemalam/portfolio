26ba504b1a15e893a4ba17be5918f0e05bdcb3e4
Nayem <nayem.alam@mail.mcgill.ca>
2019-05-12 12:09:39 +0800

    added particles.js

--- scripts/main.js
+++ scripts/main.js
@@ -77,6 +77,12 @@ $(document).ready(function () {
     }
     showNextTitle();
 
+    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+    particlesJS.load('particles-js', './assets/particles.json', function() {
+        console.log('callback - particles.js config loaded');
+    });
+
+    
     
 });
 
