7f00a65c6f408ae2739f49e61ea1a80bca44a284
Nayem <nayem.alam@mail.mcgill.ca>
2019-06-05 02:50:20 +0800

    Huge updates to about, media, speaking pages
    - about: restructured font size, added links necessary, added list toggle (read more btn for experience)
    - media: added all the links and alt for the images
    - speaking: completely redesigned the layout, arrows hide on larger displays and only show on smaller

--- scripts/about.js
+++ scripts/about.js
@@ -4,5 +4,24 @@ $(document).ready(function () {
         $('#libraries-used').toggle();
         // document.getElementById('displayDiv').style.display = 'block';
         e.preventDefault();
-    })
+    });
+
+    $('#position1').hide();
+    $('#work1-btn').click('click', function(e) {
+        $('#position1').toggle();
+        e.preventDefault();
+    });
+
+    $('#position2').hide();
+    $('#work2-btn').click('click', function(e) {
+        $('#position2').toggle();
+        e.preventDefault();
+    });
+
+    $('#position3').hide();
+    $('#work3-btn').click('click', function(e) {
+        $('#position3').toggle();
+        e.preventDefault();
+    });
+
 });
