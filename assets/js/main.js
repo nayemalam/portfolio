9708a7dab7b304b3bc0420ad0e446d227e70016f
Nayem <nayem.alam@mail.mcgill.ca>
2019-06-15 13:47:49 +0800

    massive update completed - reorganized folder structure #5 - all images have been compressed (but not local videos) #4 - lazyloaded all images #3 //TODO - put all css and js in one folder but that can be done later #2

---
+++ assets/js/main.js
@@ -0,0 +1,81 @@
+console.log("Are you a developer? Let's talk!")
+
+
+// var sidebar = document.querySelector('ul');
+// var hamburger = document.getElementsByClassName('menu-toggler');
+
+// hamburger[0].addEventListener('click', function(e){
+//     sidebar.classList.toggle('active');
+//     e.preventDefault();
+// });
+
+// $(window).load(function() {
+//     // Animate loader off screen
+//     $(".se-pre-con").fadeOut("slow");;
+// });
+
+// load preloader
+$(window).on('load', function() {
+    $(".se-pre-con").fadeOut("slow");;
+})
+$(document).ready(function () {
+    
+    // include aos framework
+    $(function() {
+        AOS.init();
+    });
+
+    $(window).on('load', function() {
+    AOS.refresh();
+    });
+    
+    // open the menu on mobile (add .active and .open class)
+    function openMenu () {
+        $(this).toggleClass('open');
+        $('ul, .social-links').toggleClass('active open');
+    }
+
+    $('.menu-toggler').on('click', openMenu);
+
+    // trigger on body to close sidenav
+    $('#main').click(function() {
+        if ($('ul, .social-links').hasClass('active open') || $('.menu-toggler').hasClass('open')) {
+          $('ul, .social-links').toggleClass('active open');
+          $('.menu-toggler').toggleClass('open');
+        }
+    });
+
+    //close hamburger menu when resizing display
+    $(window).resize(function () {
+        var windowWidth = $(window).width();
+        if(windowWidth > 868) {
+            $('ul').removeClass('active open');
+            $('.menu-toggler').removeClass('open');
+
+        } if(windowWidth==868){
+            $('.menu-toggler').removeClass('open');
+        }
+    });
+
+    // add wiggle to wave emoji
+    $('span#big').hover(function () {
+        // $(this).attr('style', 'margin: 0 auto;');
+        $(this).effect("shake", { times:2}, 1000);
+
+    });
+    
+    // Flashy Titles for Home Page
+    var titles = $(".title-na");
+    var titleIndex = -1;
+ 
+    function showNextTitle() {
+        ++titleIndex;
+        titles.eq(titleIndex % titles.length)
+        .fadeIn(2000)
+        .delay(10)
+        .fadeOut(2000, showNextTitle);
+    }
+    
+    showNextTitle();
+});
+
